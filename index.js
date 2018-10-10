9142bb56230a6ddacd52b5e73c08abe34a3b4a9d
dodoroy <effy.yin@gmail.com>
2018-10-10 15:39:54 +0800

    style fix

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 let UAUtil = {
-  //获取客户端的类型
-  isAndroidClient: function() {
-    //判断是否是安卓的客户端
-    if (navigator.userAgent.match(/Android/ig)) {
-      return true;
-    } 
-    return false;
-  },
-  isIosClient: function() {
-    //判断是否是ios的客户端
-    if (navigator.userAgent.match(/iphone|ipod|iPad/ig)) {
-      return true;
-    } 
-    return false;
-  },
-  getClientType: function() {
-    //如果是安卓的客户端返回android
-    if(navigator.userAgent.match(/Android/ig)){
-      return "android";
-    //如果是ios的客户端返回ios
-    } else if(navigator.userAgent.match(/iphone|ipod|iPad/ig)) {
-      return "ios";
-    //其他情况返回other
-    } else {
-	    return "other"; 
-	  }
-  },
-  isWechatClient: function() {
-    //判断是否是微信打开
-    if(navigator.userAgent.match(/MicroMessenger/ig)) {
-      return true;
-    }
-    return false;
-  },
+	//获取客户端的类型
+	isAndroidClient: function() {
+		//判断是否是安卓的客户端
+		if (navigator.userAgent.match(/Android/ig)) {
+			return true
+		} 
+		return false
+	},
+	isIosClient: function() {
+		//判断是否是ios的客户端
+		if (navigator.userAgent.match(/iphone|ipod|iPad/ig)) {
+			return true
+		} 
+		return false
+	},
+	getClientType: function() {
+		//如果是安卓的客户端返回android
+		if(navigator.userAgent.match(/Android/ig)){
+			return 'android'
+			//如果是ios的客户端返回ios
+		} else if(navigator.userAgent.match(/iphone|ipod|iPad/ig)) {
+			return 'ios'
+			//其他情况返回other
+		} else {
+			return 'other'
+		}
+	},
+	isWechatClient: function() {
+		//判断是否是微信打开
+		if(navigator.userAgent.match(/MicroMessenger/ig)) {
+			return true
+		}
+		return false
+	},
 	isMobile: function() {
 		//判断是否是移动端
 		if(navigator.userAgent.match(/Android|webOS|iPhone|iPod|BlackBerry|opera mini|opera mobile/ig)){
-			return true;
+			return true
 		}
-		return false;
+		return false
 	},
-  isAppClient: function(clientstr){
-    //美丽中国的ua是BeautifulChina
-    if(navigator.userAgent.indexOf(clientstr) != -1){
-      return true;
-    }
-    return false;
-  }
+	isAppClient: function(clientstr){
+		//美丽中国的ua是BeautifulChina
+		if(navigator.userAgent.indexOf(clientstr) != -1){
+			return true
+		}
+		return false
+	}
 }
 
 module.exports = UAUtil
